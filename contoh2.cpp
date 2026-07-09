d1ba8c4f4ee889f5f4ce2141baf1fc535ddc66d4
jeeyyxs <riodwimulyono0512@gmail.com>
2026-07-09 15:22:56 +0700

    membuat program utama dengan pseudocode

--- contoh2.cpp
+++ contoh2.cpp
@@ -1,3 +1,25 @@
 #include <iostream>
 using namespace std;
 
+int main() 
+{//mulai
+    //Numeric nPanjang, nLebar, nKeliling
+    int Panjang, Lebar, Keliling;
+
+    //Display "Masukkan Panjang :
+    cout << "Masukkan Panjang : ";
+    //Accept nPanjang
+    cin >> Panjang;
+    //Display "Masukkan Lebar : ";
+    cout << "Masukkan Lebar : ";
+    //Accept nLebar
+    cin >> Lebar;
+
+    //Compute Keliling
+    Keliling = 2 * (Panjang + Lebar);
+
+    //Display Keliling
+    cout << "Keliling persegi panjang adalah : " << Keliling << endl;
+    return 0;
+    
+}//selesai
