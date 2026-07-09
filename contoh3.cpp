bcf67db95eccedf8533ea4e2ef29d0785162ebff
jeeyyxs <riodwimulyono0512@gmail.com>
2026-07-09 15:27:27 +0700

    membuat program mencari keliling persegi panjang menggunakan prosedur dan fungsi

--- contoh3.cpp
+++ contoh3.cpp
@@ -1,2 +1,34 @@
 #include <iostream>
 using namespace std;
+
+//Numeric nPanjang, nLebar, nKeliling
+int Panjang, Lebar, Keliling;
+
+void Input(){
+    //Display "Masukkan Panjang : "
+    cout << "Masukkan Panjang : ";
+    //Accept nPanjang
+    cin >> Panjang;
+    //Display "Masukkan Lebar :
+    cout << "Masukkan Lebar : ";
+    //Accept nLebar
+    cin >> Lebar;
+}
+
+int Proses(){
+    //Compute nKeliling = 2(p+1)
+    return Keliling = 2 * (Panjang + Lebar);
+}
+
+void Output(){
+    //Display "Keliling Persegi Panjang : " + nKeliling
+    cout << "Keliling Persegi Panjang : " << Keliling;
+}
+
+int main()
+{//Mulai
+    Input();
+    Proses();
+    Output();
+    
+}//Selesai
